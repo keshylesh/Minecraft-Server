9ce6d5ae0b6a30631b3d57a90e27a8a202f6a133
keshylesh <keshavshylesh@gmail.com>
2025-11-19 12:51:22 -0500

    Minor fixes

--- setup/data/start.sh
+++ setup/data/start.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-java -Xms1024M -Xmx2048M -jar server.jar nogui
-sed -i "s|eula=false|eula=$EULA|" eula.txt
 
-if ! [[ -z "${SEED}" ]]; then
-    sed -i "s|level-seed=|level-seed=$SEED|" server.properties
-fi
+if ! [[ -f "server.properties" ]]; then
+    java -Xms1024M -Xmx2048M -jar server.jar nogui
+    sed -i "s|eula=false|eula=$EULA|" eula.txt
 
-if [[ "${DIFFICULTY}" == "hard" || "${DIFFICULTY}" == "normal" || "${DIFFICULTY}" == "easy" || "${DIFFICULTY}" == "peaceful" ]]; then
-    sed -i "s|difficulty=easy|difficulty=$DIFFICULTY|" server.properties
+    if ! [[ -z "${SEED}" ]]; then
+        sed -i "s|level-seed=|level-seed=$SEED|" server.properties
+    fi
+
+    if [[ "${DIFFICULTY}" == "hard" || "${DIFFICULTY}" == "normal" || "${DIFFICULTY}" == "easy" || "${DIFFICULTY}" == "peaceful" ]]; then
+        sed -i "s|difficulty=easy|difficulty=$DIFFICULTY|" server.properties
+    fi
 fi
 
 exec java -Xms1024M -Xmx2048M -jar server.jar nogui
--- setup/setup.sh
+++ setup/setup.sh
@@ -5,5 +5,6 @@ length=${#out}
 if [ "$length" == "0" ]; then
     cd "/"
     cp /setup/data/* /data
+    chmod -r +w /data
 fi
 /bin/sh /data/start.sh
